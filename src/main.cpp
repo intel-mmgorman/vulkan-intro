bd24252317227c7dc3783fac41b8f62ad233572a
meghan <meghan@weichthouse.com>
2022-06-14 17:34:42 -0500

    Code cleanup & added rendering class

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,18 @@
 #include "SDL.h"
 #include "SDL_vulkan.h"
 
+
+static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+    VkDebugUtilsMessageSeverityFlagBitsEXT message_severity,
+    VkDebugUtilsMessageTypeFlagsEXT message_type,
+    const VkDebugUtilsMessengerCallbackDataEXT* p_callback_data,
+    void* p_user_data) {
+
+    std::cerr << "validation layer: " << p_callback_data->pMessage << std::endl;
+
+    return VK_FALSE;
+}
+
 VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger) {
     auto func = (PFN_vkCreateDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
     if (func != nullptr) {
@@ -23,15 +35,65 @@ void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT
     }
 }
 
-static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
-    VkDebugUtilsMessageSeverityFlagBitsEXT message_severity,
-    VkDebugUtilsMessageTypeFlagsEXT message_type,
-    const VkDebugUtilsMessengerCallbackDataEXT* p_callback_data,
-    void* p_user_data) {
+class Renderer
+{
+    public:
+        Renderer()
+       {
+            sdl_window = {};
+            instance = {};
+            validation_layers = { "VK_LAYER_KHRONOS_validation"};
+            extensions = {};
+            debug_messenger = {};
+            enable_validation_layers = false;
+       }
+       ~Renderer()
+       {
+            if(enable_validation_layers)
+            {
+                DestroyDebugUtilsMessengerEXT(instance, debug_messenger, nullptr);
+            }
+            vkDestroyInstance(instance, nullptr);
+            SDL_DestroyWindow(sdl_window);
+            SDL_Quit();
+       }
+        //SDL
+        SDL_Window *sdl_window;
+        //Vulkan
+        VkInstance instance;
+        std::vector<const char*> validation_layers;
+        std::vector<const char*> extensions;
+        VkDebugUtilsMessengerEXT debug_messenger;
+        bool enable_validation_layers;
 
-    std::cerr << "validation layer: " << p_callback_data->pMessage << std::endl;
+        const int screen_width = 1280;
+        const int screen_height = 720;
 
-    return VK_FALSE;
+        bool initAndCreateSDLWindow();
+        bool createInstance(bool enable_validation_layers, std::vector<const char*> extensions, const std::vector<const char*> validation_layers);
+        bool setupDebugMessenger();
+        bool enableValidationLayer();
+        bool createInstance();
+        bool queryExtensions();
+
+};
+
+bool Renderer::setupDebugMessenger()
+{
+    if(enable_validation_layers)
+    {
+        VkDebugUtilsMessengerCreateInfoEXT create_info = {};
+        create_info.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+        create_info.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+        create_info.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+        create_info.pfnUserCallback = debugCallback;
+        if(CreateDebugUtilsMessengerEXT(instance, &create_info, nullptr, &debug_messenger))
+        {
+            std::cout << "Failed to set up debug messenger. " << std::endl;
+            return false;
+        }
+    }
+    return true;
 }
 
 bool checkValidationLayerSupport(const std::vector<const char*> validation_layers)
@@ -63,28 +125,8 @@ bool checkValidationLayerSupport(const std::vector<const char*> validation_layer
     return true;
 }
 
-int main(int argc, char *argv[])
+bool Renderer::enableValidationLayer()
 {
-    std::cout << "Hello World!" << std::endl;
-
-    //SDL open window
-    if(SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
-        std::cout << "SDL could not initialize! Error: " << SDL_GetError() << std::endl;
-        return 1;
-    }
-    const int screen_width = 1280;
-    const int screen_height = 720;
-    SDL_Window *window = SDL_CreateWindow("Vulkan Intro", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, screen_width, screen_height, SDL_WINDOW_SHOWN | SDL_WINDOW_VULKAN);
-    if(window == nullptr)
-    {
-        std::cout << "Could not create window: " << SDL_GetError() << std::endl;
-        return 1;
-    }
-    SDL_ShowWindow(window);
-
-    //Enable validation layers
-    const std::vector<const char*> validation_layers = { "VK_LAYER_KHRONOS_validation"};
     #ifdef NDEBUG
         const bool enable_validation_layers = false;
     #else
@@ -94,28 +136,34 @@ int main(int argc, char *argv[])
     if(enable_validation_layers && !checkValidationLayerSupport(validation_layers))
     {
         std::cout << "Validation layers requested, but not available." << std::endl;
-        return 1;
+        return false;
     }
+    return true;
+}
 
-    //Create Vulkan instance and query for extensions
+bool Renderer::queryExtensions()
+{
     unsigned int extension_count = 0;
-    if(!SDL_Vulkan_GetInstanceExtensions(window, &extension_count, nullptr))
+    if(!SDL_Vulkan_GetInstanceExtensions(sdl_window, &extension_count, nullptr))
     {
         std::cout << "Could not get Vulkan instance extensions: " <<SDL_GetError() << std::endl;
-        return 1;
+        return false;
     }
-    std::vector<const char*> extensions = {};
-    if(!SDL_Vulkan_GetInstanceExtensions(window, &extension_count, extensions.data()))
+    if(!SDL_Vulkan_GetInstanceExtensions(sdl_window, &extension_count, extensions.data()))
     {
         std::cout << "Could not get Vulkan instance extensions: " <<SDL_GetError() << std::endl;
-        return 1;
+        return false;
     }
     extensions.push_back(VK_EXT_DEBUG_REPORT_EXTENSION_NAME);
     if(enable_validation_layers)
     {
         extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
     }
+    return true;
+}
 
+bool Renderer::createInstance()
+{
     VkApplicationInfo app_info = {};
     app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
     app_info.pApplicationName = "Hello Triangle";
@@ -139,28 +187,70 @@ int main(int argc, char *argv[])
         create_instance_info.enabledLayerCount = 0;
     }
 
-    VkInstance instance = {};
     if(vkCreateInstance(&create_instance_info, nullptr, &instance) != VK_SUCCESS)
     {
         std::cout << "Failed to create Vulkan instance." << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+bool Renderer::initAndCreateSDLWindow()
+{
+    //SDL open window
+    if(SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        std::cout << "SDL could not initialize! Error: " << SDL_GetError() << std::endl;
+        return false;
+    }
+
+    sdl_window = SDL_CreateWindow("Vulkan Intro", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, screen_width, screen_height, SDL_WINDOW_SHOWN | SDL_WINDOW_VULKAN);
+    if(sdl_window == nullptr)
+    {
+        std::cout << "Could not create window: " << SDL_GetError() << std::endl;
+        return false;
+    }
+    return true;
+}
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Hello World!" << std::endl;
+
+    Renderer renderer = {};
+    bool result = renderer.initAndCreateSDLWindow();
+    if(!result)
+    {
         return 1;
     }
 
-    //Set up debug messenger
-    VkDebugUtilsMessengerEXT debug_messenger = {};
-    if(enable_validation_layers)
-    {
-        VkDebugUtilsMessengerCreateInfoEXT create_info = {};
-        create_info.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
-        create_info.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
-        create_info.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
-        create_info.pfnUserCallback = debugCallback;
-        create_info.pUserData = nullptr; // Optional
+    SDL_ShowWindow(renderer.sdl_window);
 
-        if(CreateDebugUtilsMessengerEXT(instance, &create_info, nullptr, &debug_messenger))
-        {
-            std::cout << "Failed to set up debug messenger. " << std::endl;
-        }
+    //Enable validation layers
+    result = renderer.enableValidationLayer();
+    if(!result)
+    {
+        return 1;
+    }
+
+    //Create Vulkan instance and query for extensions
+    result = renderer.queryExtensions();
+    if(!result)
+    {
+        return 1;
+    }
+
+    result = renderer.createInstance();
+    if(!result)
+    {
+        return 1;
+    }
+
+    result = renderer.setupDebugMessenger();
+    if(!result)
+    {
+        return 1;
     }
 
     bool running = true;
@@ -176,14 +266,4 @@ int main(int argc, char *argv[])
                 break;
         }
     }
-
-    // Quit application
-    if(enable_validation_layers)
-    {
-        DestroyDebugUtilsMessengerEXT(instance, debug_messenger, nullptr);
-    }
-    vkDestroyInstance(instance, nullptr);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    return 0;
 }
