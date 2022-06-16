9ecf5fa75cb5d23961c6955e5919fbf830465306
meghan <meghan@weichthouse.com>
2022-06-16 11:34:42 -0500

    Created presentation queue

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <vector>
+#include <set>
 #include <cstring>
 #include <vulkan/vulkan.h>
 #include "SDL.h"
@@ -85,6 +86,7 @@ class Renderer
         VkDevice device;
         VkQueue graphics_queue;
         VkSurfaceKHR surface;
+        VkQueue present_queue;
 
         const int screen_width = 1280;
         const int screen_height = 720;
@@ -274,19 +276,26 @@ bool Renderer::initAndCreateSDLWindow()
 
 bool Renderer::createLogicalDevice()
 {
-    VkDeviceQueueCreateInfo queue_create_info = {};
-    queue_create_info.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
-    queue_create_info.queueFamilyIndex = graphics_family;
-    queue_create_info.queueCount = 1;
+    std::vector<VkDeviceQueueCreateInfo> queue_create_infos = {};
+    std::set<uint32_t> unique_queue_families = {graphics_family, present_family};
     const float queue_priority = 1.0f;
-    queue_create_info.pQueuePriorities = &queue_priority;
+    for(uint32_t queue_family : unique_queue_families)
+    {
+        VkDeviceQueueCreateInfo queue_create_info = {};
+        queue_create_info.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+        queue_create_info.queueFamilyIndex = queue_family;
+        queue_create_info.queueCount = 1;
+        queue_create_info.pQueuePriorities = &queue_priority;
+        queue_create_infos.push_back(queue_create_info);
+    }
+
 
     VkPhysicalDeviceFeatures device_features = {};
 
     VkDeviceCreateInfo create_info = {};
     create_info.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
-    create_info.pQueueCreateInfos = &queue_create_info;
-    create_info.queueCreateInfoCount = 1;
+    create_info.pQueueCreateInfos = queue_create_infos.data();
+    create_info.queueCreateInfoCount = static_cast<uint32_t>(queue_create_infos.size());
     create_info.pEnabledFeatures = &device_features;
     create_info.enabledExtensionCount = 0;
 
@@ -307,6 +316,7 @@ bool Renderer::createLogicalDevice()
     }
 
     vkGetDeviceQueue(device, graphics_family, 0, &graphics_queue);
+    vkGetDeviceQueue(device, present_family, 0, &present_queue);
 
     return true;
 }
