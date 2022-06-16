5b25aab0e62be365a954c36e417400d7206f33ee
meghan <meghan@weichthouse.com>
2022-06-16 17:26:48 -0500

    Choose swap present mode

--- src/main.cpp
+++ src/main.cpp
@@ -139,6 +139,18 @@ VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>
     return available_formats[0];
 }
 
+VkPresentModeKHR chooseSwapPresentMode(const std::vector<VkPresentModeKHR>& available_present_modes)
+{
+    for(const auto& available_present_mode : available_present_modes)
+    {
+        if(available_present_mode == VK_PRESENT_MODE_MAILBOX_KHR)
+        {
+            return available_present_mode;
+        }
+    }
+    return VK_PRESENT_MODE_FIFO_KHR;
+}
+
 SwapChainSupportDetails Renderer::querySwapChainSupport(VkPhysicalDevice device)
 {
     SwapChainSupportDetails details = {};
